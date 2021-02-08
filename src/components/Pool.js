897da09c46fec627896e919a002f25f16a717a9e
Jack Sanders <Guitarbreakerjs@gmail.com>
2021-02-08 15:02:35 -0700

    pool words

--- src/components/Pool.js
+++ src/components/Pool.js
@@ -3,7 +3,14 @@ import poolPic from '../assets/pool2.png';
 export function Pool(props) {
   return (     
       <div className="poolContainer">
+        <div className= "poolWordsContainer"> 
+          <span className="pictureWords" id="pool_char1">B</span> 
+          <span className="pictureWords" id="pool_char2">L</span> 
+          <span className="pictureWords" id="pool_char3">O</span> 
+          <span className="pictureWords" id="pool_char4">G</span> 
+        </div>
         <img src={poolPic} className="pool" alt="logo"/>
       </div>
     );
 }
+
