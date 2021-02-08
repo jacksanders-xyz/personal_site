6bc171a7402f560500cde39f1b86709da0ee59fa
Jack Sanders <Guitarbreakerjs@gmail.com>
2021-02-07 23:18:53 -0700

    full on hands dude

--- src/components/ShakingHands.js
+++ src/components/ShakingHands.js
@@ -3,7 +3,9 @@ import shakingHandsPic from '../assets/shakinghands.png';
 export function ShakingHands(props) {
   return (     
     <div className="shakinghandsContainer">
-    <img src={shakingHandsPic} className="shakinghands" alt="logo"/>
+      <div className="shakinghandsSubContainer">
+        <img src={shakingHandsPic} className="shakinghands" alt="logo"/>
+      </div>
     </div>
     );
 }
