f61403ab80050b3dc2f389aa01d166f66f40f98b
Jack Sanders <Guitarbreakerjs@gmail.com>
2021-02-07 18:39:54 -0700

    componentarized

--- src/App.js
+++ src/App.js
@@ -1,34 +1,12 @@
 import { Component } from 'react';
-import computerJack from './assets/computerJack.jpg';
+import { LittleDude } from './components/LittleDude';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-      <div className="mainContainer">
-        <img src={computerJack} className="App-logo" alt="logo"/>
-        <span className="jackNameWrap" id="char1">J</span>
-        <span className="jackNameWrap" id="char2">A</span>
-        <span className="jackNameWrap" id="char3">C</span>
-        <span className="jackNameWrap" id="char4">K</span>
-        <span className="jackNameWrap" id="char5">C</span>
-        <span className="jackNameWrap" id="char6">A</span>
-        <span className="jackNameWrap" id="char7">L</span>
-        <span className="jackNameWrap" id="char8">L</span>
-        <span className="jackNameWrap" id="char9">A</span>
-        <span className="jackNameWrap" id="char10">W</span>
-        <span className="jackNameWrap" id="char11">A</span>
-        <span className="jackNameWrap" id="char12">Y</span>
-        <span className="jackNameWrap" id="char13">S</span>
-        <span className="jackNameWrap" id="char14">A</span>
-        <span className="jackNameWrap" id="char15">N</span>
-        <span className="jackNameWrap" id="char16">D</span>
-        <span className="jackNameWrap" id="char17">E</span>
-        <span className="jackNameWrap" id="char18">R</span>
-        <span className="jackNameWrap" id="char19">S</span>
-      </div>
-
+        <LittleDude />
       </div>
     );
   }
---
+++ src/components/LittleDude.js
@@ -0,0 +1,28 @@
+import computerJack from '../assets/computerJack.jpg';
+
+export function LittleDude(props) {
+  return (     
+    <div className="mainContainer">
+          <img src={computerJack} className="App-logo" alt="logo"/>
+          <span className="jackNameWrap" id="char1">J</span>
+          <span className="jackNameWrap" id="char2">A</span>
+          <span className="jackNameWrap" id="char3">C</span>
+          <span className="jackNameWrap" id="char4">K</span>
+          <span className="jackNameWrap" id="char5">C</span>
+          <span className="jackNameWrap" id="char6">A</span>
+          <span className="jackNameWrap" id="char7">L</span>
+          <span className="jackNameWrap" id="char8">L</span>
+          <span className="jackNameWrap" id="char9">A</span>
+          <span className="jackNameWrap" id="char10">W</span>
+          <span className="jackNameWrap" id="char11">A</span>
+          <span className="jackNameWrap" id="char12">Y</span>
+          <span className="jackNameWrap" id="char13">S</span>
+          <span className="jackNameWrap" id="char14">A</span>
+          <span className="jackNameWrap" id="char15">N</span>
+          <span className="jackNameWrap" id="char16">D</span>
+          <span className="jackNameWrap" id="char17">E</span>
+          <span className="jackNameWrap" id="char18">R</span>
+          <span className="jackNameWrap" id="char19">S</span>
+        </div>
+    );
+}
