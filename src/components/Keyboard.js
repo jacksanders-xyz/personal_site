6e57eaaa7de1ce36115b58cbee409d0c30a6afdd
Jack Sanders <Guitarbreakerjs@gmail.com>
2021-02-08 15:16:38 -0700

    all the words

--- src/components/Keyboard.js
+++ src/components/Keyboard.js
@@ -3,6 +3,7 @@ import KeyboardPic from '../assets/Keyboard2.png';
 export function Keyboard(props) {
   return (     
       <div className="keyboardContainer">
+      <div className="keyboardWordsContainer">CHAT</div>
         <img src={KeyboardPic} className="keyboard" alt="logo"/>
       </div>
     );
