f57177fd4523f9edd74bea646843fdba76371abb
Jack Sanders <Guitarbreakerjs@gmail.com>
2021-02-10 23:54:45 -0700

    .env

--- src/components/Keyboard.js
+++ src/components/Keyboard.js
@@ -1,10 +1,12 @@
 import KeyboardPic from '../assets/Keyboard2.png';
+const dotenv = require("dotenv") 
+dotenv.config()
 
 export function Keyboard(props) {
   return (     
       <div className="keyboardContainer">
         <div className="keyboardWordsContainer">CHAT</div>
-        <img src={KeyboardPic}  className="keyboard" alt="logo"/>
+        <img src={KeyboardPic} onClick={() => console.log(process.env.REACT_APP_fun_message) } className="keyboard" alt="logo"/>
       </div>
     );
 }
